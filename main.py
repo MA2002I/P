4b1e597a28dacf7cd422ff305f3658c22a03ee1e
MA2002I <mjini2003@gmail.com>
2025-01-06 00:09:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-print("Welcome to the YouTube Channel Name Generator: ")
-name = input("What is your nickname? \n")
-topic = input("What is your channel about? \n")
-print ("You could name your channel " + "(" + topic + " with " + name + ")")
+Length = input("Pleas type Length : \n")
+Width = input("Pleas type Width : \n")
+Price = input("how mcuh for 1 meter? : \n")
+#تحويل البيانات النصية الى ارقام عشرية
+str_Length =float(Length)
+str_Width = float(Width)
+str_Price = float(Price)
+#اقوم اسوي العمليات الحسابية
+Area = str_Length*str_Width 
+Pricetootl= Area *str_Price 
+#ارجع احول الارقام الى نصوص عشلن اطبعهم
+str_area= str(Area)
+str_Pricetootl=str(Pricetootl)
+#نبدا بالطباعة
+print("The total area is :" + str_area )
+print("Give the guy :" + str_Pricetootl + "$")
